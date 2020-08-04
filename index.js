ccef34fcc13b4a473c39f8e05031d76658fadcb7
Bhavik Prajapati <66296381+iambhavik11@users.noreply.github.com>
2020-08-04 15:31:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+let myForm = document.querySelector('#my-Form');
+let myEmail = document.querySelector('#Email');
+let myPassword = document.querySelector('#passWord');
+let msgError1 = document.querySelector('#span1');
+let msgError2 = document.querySelector('#span2');
+let msgSuccess = document.querySelector('#span3');
+
+
+msgError1.innerHTML = '<small>(eg. example@gmail.com)</small>';
+msgError1.style.color = 'grey';
+
+msgError2.innerHTML = '<small>(eg. It can be mixture of 0-9 , A-z , a-z , @ , $ are allowed)</small>';
+msgError2.style.color = 'grey';
+
+myForm.addEventListener('submit',onSubmit);
+myForm.addEventListener('reset',onreset);
+
+function onSubmit(){
+    event.preventDefault();
+    if(myEmail.value === '' && myPassword.value === ''){
+        msgError1.innerHTML ='<small>Please fill the input field!</small>';
+        msgError1.style.color = 'red';
+
+        msgError2.innerHTML ='<small>Please fill the input field!</small>';
+        msgError2.style.color = 'red';
+    }
+    else if(!(myEmail.value === '') && (myPassword.value === '')){
+        msgError1.innerHTML = '<small>(eg. example@gmail.com)</small>';
+        msgError1.style.color = 'grey';
+        msgError2.innerHTML ='<small>Please fill the input field!</small>';
+        msgError2.style.color = 'red';
+    }
+    
+    else if((myEmail.value === '') && !(myPassword.value === '')){
+        msgError1.innerHTML ='<small>Please fill the input field!</small>';
+        msgError1.style.color = 'red';
+        msgError2.innerHTML = '<small>(eg. It can be mixture of 0-9 , A-z , a-z , @ , $ are allowed)</small>';
+        msgError2.style.color = 'grey';
+    }
+    else {
+        msgError1.innerHTML = '<small>(eg. example@gmail.com)</small>';
+        msgError1.style.color = 'grey';
+        msgError2.innerHTML = '<small>(eg. It can be mixture of 0-9 , A-z , a-z , @ , $ are allowed)</small>';
+        msgError2.style.color = 'grey';
+        msgSuccess.innerHTML ='<center>SignUp Successfull!!</center>';
+        msgSuccess.style.color = 'green';
+    }
+    
+   
+}
+
+function onreset(){
+    myEmail.value = '';
+    myPassword.value ='';
+}
